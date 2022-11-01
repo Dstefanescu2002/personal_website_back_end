c0cee3632aab0d547ae7ee78b70da6a1467122e4
Dstefanescu2002 <dstefanescu1609@gmail.com>
2022-11-01 17:39:50 -0400

    Created a wrapper class for daniel bot to make code much neater and callable from main

--- daniel_bot.py
+++ daniel_bot.py
@@ -3,31 +3,44 @@ import requests
 import os
 from config import HF_API_KEY
 
-def query(payload='',parameters=None,options={'use_cache': False}):
-    API_URL = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B"
-    headers = {"Authorization": f"Bearer {HF_API_KEY}"}
-    body = {"inputs":payload,'parameters':parameters,'options':options}
-    response = requests.request("POST", API_URL, headers=headers, data= json.dumps(body))
-    try:
-      response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        return "Error:"+" ".join(response.json()['error'])
-    else:
-      return response.json()[0]['generated_text']
+class DanielBot():
+    def __init__(self):
+        self.__parameters= {
+            'max_new_tokens':25,  # number of generated tokens
+            'temperature': 0.5,   # controlling the randomness of generations
+            'end_sequence': "###" # stopping sequence for generation
+        }
+        with open('./daniel_info.txt') as f:
+            daniel_info = ''.join(f.readlines())
+        sample_qa = '''
+            Q: Do you play any sports?\nA: Yes, I play soccer\n###\n
+            Q: Where did you go to college?\nA: I went to The University of Michigan, Ann Arbor\n###\n
+        '''
+        self.__context = f'''
+            Based on the context below the model can answers questions by extracting contiguos portion of the text.\n
+            C:"{daniel_info}"\n\n
+            {sample_qa}
+        '''
 
-parameters = {
-    'max_new_tokens':25,  # number of generated tokens
-    'temperature': 0.5,   # controlling the randomness of generations
-    'end_sequence': "###" # stopping sequence for generation
-}
+    def __gpt3_query(self, payload='',options={'use_cache': False}):
+        API_URL = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B"
+        headers = {"Authorization": f"Bearer {HF_API_KEY}"}
+        body = {"inputs":payload,'parameters':self.__parameters,'options':options}
+        response = requests.request("POST", API_URL, headers=headers, data= json.dumps(body))
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            return "Error:"+" ".join(response.json()['error'])
+        else:
+            return response.json()[0]['generated_text']
+    
+    def get_response(self, question: str) -> str:
+        formatted_question = self.__context + f'Q: {question}\nA: '
+        data = self.__gpt3_query(formatted_question)
+        trimmed = data[len(formatted_question):]
+        return trimmed[:trimmed.index('###')].strip()
 
-context  = ''
-with open('./daniel_info.txt') as f:
-    context += ''.join(f.readlines())
-
-question = input('Type a question: ')
-sample_qa = 'Q: Do you play any sports?\nA: Yes, I play soccer\n###\nQ: Where did you go to college?\nA: I went to The University of Michigan, Ann Arbor\n###\n'
-prompt = f'Based on the context below the model can answers questions by extracting contiguos portion of the text.\nC:"{context}"\n\n{sample_qa}Q: {question}\nA:'
-data = query(prompt,parameters)
-trimmed = data[len(prompt)-2:]
-print (trimmed[:trimmed.index('\n')])
+if __name__ == "__main__":
+    db = DanielBot()
+    question = input("Ask me a question: ")
+    print(db.get_response(question))
